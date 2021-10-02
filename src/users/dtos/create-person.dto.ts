52697106d8ced08c1bc6a2f79203f51a402115ab
Nati Kamusher <natan.kamusher@gmail.com>
2021-10-02 10:34:28 +0300

    added commantary

--- src/users/dtos/create-person.dto.ts
+++ src/users/dtos/create-person.dto.ts
@@ -6,8 +6,8 @@ export class CreatePersonReqDto {
   @Expose()
   @IsNotEmpty()
   @ValidateNested()
-  @Type(() => {
-    return PersonalDetailsDto;
-  })
+  @Type(() => PersonalDetailsDto)
+  // Nested fields are not exposed automatically,
+  // a field should have at least one class-validator decorator to be visible
   personalDetails: PersonalDetailsDto;
 }
--- src/users/dtos/personalDetails.dto.ts
+++ src/users/dtos/personalDetails.dto.ts
@@ -1,9 +1,10 @@
 import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
 import { Expose } from 'class-transformer';
 export class PersonalDetailsDto {
+  // Nested fields are not exposed automatically,
+  // a field should have at least one class-validator decorator to be visible
   @IsNumber()
   @IsNotEmpty()
-  @Expose()
   age: number;
 
   @IsString()
